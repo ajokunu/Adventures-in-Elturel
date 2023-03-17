38858aef2aec0842239d09df40818ee03ab164f6
Justus Lindahl <65878280+ajokunu@users.noreply.github.com>
2023-03-17 12:12:19 -0400

    YEP
    Broken will fix later

--- Elturel.py
+++ Elturel.py
@@ -11,9 +11,19 @@ class TreeNode:
   def traverse_story(self):
      story_node = self
      print(story_node.story_piece)
+     while  len(story_node.choices) > 0:
+       choice = input("Enter 1 or 2 to continue the story:")
+       if choice not in ["1" , "2"]:
+         print("Please use a braincell, the fate of Elturel depends on this!")
+         chosen_index = int(choice)
+         chosen_index -= 1
+         chosen_child = story_node.choices[chosen_index]
+         print(chosen_child.story_piece)
+         story_node = chosen_child
+
 #opening moves
 story_root = TreeNode("""
-You find yourself in the cook cellar belonging to the guild of The Seven, having just finished a salty pork shank and a glass of ale you when hear shouts erupt from the streets outside and feel the ground shake beneath your feet.
+You find yourself in the cook cellar belonging to the guild of The Seven, having just finished a salty pork shank and a glass of ale when hear shouts erupt from the streets outside and feel the ground shake beneath your feet.
 Do you: 
 1 ) Race Outside!
 2 ) Finish your boiled cabbage.
@@ -37,7 +47,8 @@ Do you:
 """)
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
-story_root.traverse_story()
+
 #prints for testing
-print("Once upon a time in the city of Elturel, home to vagrants, priests and vivacious villains alike a pair of chains and a floating sun ruined a perfectly good breakfast.")
+print("Once upon a time in the city of Elturel, home to vagrants, priests and vivacious villains alike a bunch of chains and a floating sun ruined a perfectly good breakfast.")
 print(story_root.story_piece)
+story_root.traverse_story()
